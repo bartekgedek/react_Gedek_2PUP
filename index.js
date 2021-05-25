429b221da1c21bac982482024b285ab528a30b32
bartekgedek <79149489+bartekgedek@users.noreply.github.com>
2021-05-25 14:49:34 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react'; // import potrzebnych bibliotek
+import ReactDOM from 'react-dom';  // import potrzebnych bibliotek
+
+import MyFirstComponent from './1/myFirstComponent';
+
+
+
+
+ReactDOM.render(<div>
+    <MyFirstComponent />
+</div>, document.getElementById('app'));
+
