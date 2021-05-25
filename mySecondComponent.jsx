4fefaecb143199b1e9a2dae4a56590780e2f32f1
bartekgedek <79149489+bartekgedek@users.noreply.github.com>
2021-05-25 14:52:14 +0200

    components

---
+++ mySecondComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MySecondComponent extends React.Component { 
+  constructor(props) { 
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Hello!
+      </div>
+    );
+  }
+}
+
+export default MySecondComponent;
+
