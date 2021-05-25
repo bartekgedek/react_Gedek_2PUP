9c059fcf5ecf954dcc8182b8ddfb745fff7be1e8
bartekgedek <79149489+bartekgedek@users.noreply.github.com>
2021-05-25 14:59:09 +0200

    index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import React from 'react'; // import potrzebnych bibliotek
 import ReactDOM from 'react-dom';  // import potrzebnych bibliotek
 
-import MyFirstComponent from './1/myFirstComponent';
+import MyFirstComponent from './myFirstComponent';
+import MySecondComponent from './mySecondComponent';
 
 
 
 
 ReactDOM.render(<div>
     <MyFirstComponent />
+    <MySecondComponent/>            
 </div>, document.getElementById('app'));
 
